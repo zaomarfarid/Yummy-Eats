48139dc2738f913cc7a6286429a1cbfd873c1fce
Omar Farid <omarfarid222@gmail.com>
2021-01-03 18:15:36 +0200

    final touch up

--- script/app.js
+++ script/app.js
@@ -1,27 +1,5 @@
-let btn = document.querySelector('#cta-btn');
-let overlay = document.querySelector('#overlay');
-
-// It will work too, I was just playing around :)
-
-// const animate = () => (
-//     new Promise((resolve) => {
-//         overlay.style.display = 'grid';
-//         overlay.classList.add('animate-overlay');
-//         setTimeout(() => {
-//             overlay.style.display = 'none';
-//             overlay.classList.remove('animate-overlay');
-//         }, 3000);
-//         resolve();
-//     })
-// );
-
-// btn.addEventListener('click', async () => {
-//     await animate()
-//         .then(() => animate())
-//         .catch((err) => {
-//             console.log('error!', err)
-//         });
-// });
+const btn = document.querySelector('#cta-btn');
+const overlay = document.querySelector('#overlay');
 
 btn.addEventListener('click', () => {
     overlay.style.display = 'grid';
