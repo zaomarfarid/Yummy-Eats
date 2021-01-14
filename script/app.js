99cc0654b14bf70d372af0f8dd35b2cc2ac18b56
Omar Farid <omarfarid222@gmail.com>
2021-01-14 19:47:55 +0200

    results init

--- script/app.js
+++ script/app.js
@@ -1,11 +1,13 @@
 const btn = document.querySelector('#cta-btn');
 const overlay = document.querySelector('#overlay');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', async (e) => {
+    e.preventDefault();
     overlay.style.display = 'grid';
     overlay.classList.add('animate-overlay');
     setTimeout(() => {
         overlay.style.display = 'none';
         overlay.classList.remove('animate-overlay');
+        window.location.replace('/results.html');
     }, 3000);
 });
