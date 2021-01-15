e23a808f1c9ca5b50324dd5c8ec630d274ee6c61
Omar Farid <omarfarid222@gmail.com>
2021-01-15 13:40:58 +0200

    templeting

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 # Configure application
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 @app.route('/')
 def index():
     return render_template('index.html')
--- flaskr/static/app.js
+++ flaskr/static/app.js
@@ -1,13 +1,11 @@
 const btn = document.querySelector('#cta-btn');
 const overlay = document.querySelector('#overlay');
+const form = document.querySelector('form');
 
 btn.addEventListener('click', async (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     overlay.style.display = 'grid';
     overlay.classList.add('animate-overlay');
-    setTimeout(() => {
-        overlay.style.display = 'none';
-        overlay.classList.remove('animate-overlay');
-        // window.location.replace('/search');
-    }, 3000);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    form.submit();
 });
