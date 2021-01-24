d417fbeb61ac33c361b704983c2e6b1663c1d6d3
Omar Farid <omarfarid222@gmail.com>
2021-01-24 17:38:00 +0200

    odm

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const path = require('path');
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+// const { City } = require('./models/city');
+const port = 3000;
 
 // connect to mongoDB using mongooose
 mongoose.connect('mongodb://localhost:27017/eats', {
@@ -16,18 +18,38 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => { console.log('Database connected') });
 
+// food Schema
+const citySchema = new Schema({
+    name: String, // String is shorthand for {type: String}
+    food: [{ name: String, price: Number }],
+    body: String,
+    comments: [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: {
+        votes: Number,
+        favs: Number
+    }
+});
+
+const City = mongoose.model('City', citySchema);
+
+// City.insertMany([
+//     { name: 'Wuhan', food: [{ name: 'noddles', price: 15 }, { name: 'macaroni', price: 10 }] },
+//     { name: 'San Diego', food: [{ name: 'something', price: 25 }, { name: 'rice', price: 20 }] }
+// ]);
+
+// const wuhan = new City();
+// wuhan.save();
+
 const app = express();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-// useless - default
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(__dirname));
 
 // to parse req.body
 app.use(express.urlencoded({ extended: true }));
-
-
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
@@ -35,9 +57,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
-    res.render('results');
+    res.render('results')
 })
 
-app.listen(3000, () => {
-    console.log('LISTENING ON PORT 3000')
+app.post('/search', async (req, res) => {
+    const name = req.body.city;
+    const city = await City.findOne({ name });
+    res.redirect(`/search/${city.id}`);
+})
+
+app.get('/search/:id', async (req, res) => {
+    const city = await City.findById(req.params.id);
+    res.render('results', { city })
+})
+
+app.listen(port, () => {
+    console.log(`Listening to http://localhost:${port}`)
 })
--- script/main.js
+++ script/main.js
@@ -1,11 +1,13 @@
 const btn = document.querySelector('#cta-btn');
 const overlay = document.querySelector('#overlay');
+const form = document.querySelector('form');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
     overlay.style.display = 'grid';
     overlay.classList.add('animate-overlay');
-    setTimeout(() => {
-        overlay.style.display = 'none';
-        overlay.classList.remove('animate-overlay');
-    }, 3000);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    overlay.style.display = 'none';
+    overlay.classList.remove('animate-overlay');
+    form.submit();
 });
