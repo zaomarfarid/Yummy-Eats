76624f1975c3b2eb99d67971671b9aeaa275d899
Omar Farid <omarfarid222@gmail.com>
2021-01-31 23:51:48 +0200

    refactor js

--- script/app.js
+++ script/app.js
@@ -1,11 +1,10 @@
 const btn = document.querySelector('#cta-btn');
 const overlay = document.querySelector('#overlay');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', async () => {
     overlay.style.display = 'grid';
     overlay.classList.add('animate-overlay');
-    setTimeout(() => {
-        overlay.style.display = 'none';
-        overlay.classList.remove('animate-overlay');
-    }, 3000);
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    overlay.style.display = 'none';
+    overlay.classList.remove('animate-overlay');
 });
